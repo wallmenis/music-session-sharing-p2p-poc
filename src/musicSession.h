04e58e02e08f03a7a614c346782f280f2e900f49
Dimostheni Krallis <dimosthenis.krallis@outlook.com>
2024-11-05 18:32:38 +0200

    added some files that will be useful

---
+++ src/musicSession.h
@@ -0,0 +1,16 @@
+#ifndef MUSICSESSION_H
+#define MUSICSESSION_H
+#endif
+
+#include "rtc/rtc.hpp"
+
+
+#include <chrono>
+#include <future>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <unordered_map>
+#include <nlohmann/json.hpp>
+
+
